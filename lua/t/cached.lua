efde6d7960fa50468a63248f2d8a080f53e9c477
luatoolz <luatoolz@somahorse.com>
2024-07-01 17:13:23 +0300

    add: cached

---
+++ lua/t/cached.lua
@@ -0,0 +1,23 @@
+local t = require "t"
+local noop = t.fn.noop
+
+local index={}
+local cached = {}
+return setmetatable(cached, {
+  __newindex=function(self, it, v) rawget(self, tonumber(it), v) end,
+  __index=function(self, it) return rawget(self, tonumber(it)) end,
+  __call=function(self, it)
+    if self==cached then
+      local rv = setmetatable({}, getmetatable(self))
+      if type(it)=='function' then
+        index[rv]=it
+      else
+        index[rv]=noop
+      end
+      return rv
+    end
+    assert(type(it)=='table')
+    if not self[it] then self[it]=it end
+    return it
+  end,
+})
