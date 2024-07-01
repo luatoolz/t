e4daccc36c2b7cbac479d1076a47b265f6f5ea63
luatoolz <luatoolz@somahorse.com>
2024-07-01 06:58:10 +0300

    fix is.z

--- lua/t/is/z.lua
+++ lua/t/is/z.lua
@@ -2,6 +2,6 @@ return function(x)
   return  type(x)=='nil' or
           x==false or
           x==0 or x=='0' or
-          x=="" or 
-          ((type(x)=='string' and x:match("^%s+$")~=nil) and true or false) or 
+          x=="" or
+          ((type(x)=='string' and x:match("^%s+$")~=nil) and true or false) or
           (type(x)=='table' and type(next(x))=='nil') end
