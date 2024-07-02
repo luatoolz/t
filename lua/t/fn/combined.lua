a17cc67496a62108657f8db1ba2cc26bdc6a4609
luatoolz <luatoolz@somahorse.com>
2024-07-02 07:50:40 +0300

    fixes: .busted fn/combined

--- lua/t/fn/combined.lua
+++ lua/t/fn/combined.lua
@@ -4,7 +4,7 @@ local is = t.is
 local unpak = unpack or table.unpack
 local pak = pack or table.pack
 return function(...)
-  local func={...}
+  local func=pak(...)
   return function(...)
     local rv=pak(...)
     for i,f in ipairs(func) do
