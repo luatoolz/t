134f465d76320c220db62bb32df2d757d69b3bf5
luatoolz <luatoolz@somahorse.com>
2024-07-02 08:06:42 +0300

    fix combined

--- lua/t/fn/combined.lua
+++ lua/t/fn/combined.lua
@@ -1,14 +1,16 @@
 require 'compat53'
 local t = require "t"
 local is = t.is
+local unpak = unpack or table.unpack
+local pak = pack or table.pack
 return function(...)
   local func={...}
-  return function(x)
-    local rv=x
+  return function(...)
+    local rv=pak(...)
     for i,f in ipairs(func) do
       assert(is.callable(f))
-      rv=f(rv)
+      rv=pak(f(unpak(rv)))
     end
-    return rv
+    return unpak(rv)
   end
 end
