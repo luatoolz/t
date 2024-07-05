3f2b3a78651b5f44316d2e56eb693c64928aad1a
luatoolz <luatoolz@somahorse.com>
2024-07-05 08:00:53 +0300

    fix: t.object __index -> __preindex __postindex

--- lua/t/object.lua
+++ lua/t/object.lua
@@ -13,7 +13,8 @@ local tables = table{'__computed', '__computable', '__imports'}:tohash()
 
   return t.object({...})
         :imports({...})
-        :index({...})
+        :preindex({...})
+        :postindex({...})
         :computed(...)
         :loader({...})
         :factory({})
@@ -40,10 +41,10 @@ return mt({}, {
     if it then cache.loader[self.tt]=it end
     return self
   end,
-  index       = function(self, f)                                       -- set __index function
-    if is.callable(f) then self.__indexed=f end
-    return self
-  end,
+  preindex    = function(self, f)                                       -- set __preindex function
+    if is.callable(f) then self.__preindex=f end; return self; end,
+  postindex       = function(self, f)                                   -- set __postindex function
+    if is.callable(f) then self.__postindex=f end; return self; end,
   factory     = function(this, t)                                       -- return factory
     return mt(this.tt:mtremove(t), this.mm:mtremove({
       __index=no.object,
