aa338c32d62919e655887d51355210096cd27654
luatoolz <luatoolz@somahorse.com>
2024-07-26 01:46:43 +0300

    reorder array/set meta methods

--- lua/t/array.lua
+++ lua/t/array.lua
@@ -8,16 +8,16 @@ local args, iter = table.args, table.iter
 local array
 array = setmetatable({
   of=table.of,
-  flatten=function(self, o) return is.array(self) and self(o and table.flatten(o) or nil) or array(table.flatten(self)) end,
+  flatten=function(self, o) return array(table.flatten((is.array(self) and o) and o or self)) end,
 },{
---  __item=t.fn.noop,
-  __mod=function(self, it) if type(it)=='function' then return self(table.filter(self, it)) end end,
-  __mul=function(self, it) if type(it)=='function' then return self(table.map(self, it)) end; return self end,
-  __iter=table.ivalues,
-  __pairs=ipairs,
-  __tostring=function(self)
-    return table.concat(self, "\n")
+  __add=function(self, it)
+    assert(is.mtequal(t.array, self, {'__item'}))
+    if is.bulk(it) then return self .. it end
+    it=mt(self).__item and mt(self).__item(it) or it
+    if it then table.insert(self, it) end
+    return self
   end,
+  __array=true,
   __call=function(self, ...)
     assert(is.mtequal(t.array, self, {'__item'}))
     assert(is.callable(mt(self).__item) or mt(self).__item==nil)
@@ -28,24 +28,6 @@ array = setmetatable({
     if is.bulk(o) then for it in iter(o) do _=self+it end end
     return self
   end,
-  __add=function(self, it)
-    assert(is.mtequal(t.array, self, {'__item'}))
-    if is.bulk(it) then return self .. it end
-    it=mt(self).__item and mt(self).__item(it) or it
-    if it then table.insert(self, it) end
-    return self
-  end,
-  __sub=function(self, it)
-    assert(is.mtequal(t.array, self, {'__item'}))
-    if is.bulk(it) then for o in iter(it) do _=self-o end end
-    if it and type(it)=='number' then
-      if it<1 or it>#self then it=nil end
-      table.remove(self, it) end
-    return self
-  end,
-  __unm=function(self)
-    return #self>0 and table.remove(self) or nil
-  end,
   __eq=function(a, b)
     assert(is.mtequal(t.array, a, {'__item'}))
     assert(type(a)=='table' and type(b)=='table')
@@ -55,5 +37,20 @@ array = setmetatable({
     if type(table.keys(a)())~='nil' or type(table.keys(b)())~='nil' then return false end
     return true
   end,
+--  __item=t.fn.noop,
+  __iter=table.ivalues,
+  __mod=function(self, it) if type(it)=='function' then return self(table.filter(self, it)) end end,
+  __mul=function(self, it) if type(it)=='function' then return self(table.map(self, it)) end; return self end,
+  __pairs=ipairs,
+  __sub=function(self, it)
+    assert(is.mtequal(t.array, self, {'__item'}))
+    if is.bulk(it) then for o in iter(it) do _=self-o end end
+    if it and type(it)=='number' then
+      if it<1 or it>#self then it=nil end
+      table.remove(self, it) end
+    return self
+  end,
+  __tostring=function(self) return table.concat(self, "\n") end,
+  __unm=function(self) return #self>0 and table.remove(self) or nil end,
 })
 return array
--- lua/t/set.lua
+++ lua/t/set.lua
@@ -8,6 +8,33 @@ local args, iter = table.args, table.iter
 return setmetatable({
   of=table.of,
 },{
+  __add=function(self, it)
+    assert(is.of.set(self))
+    if not it then return self end
+    if is.bulk(it) then return self .. it end
+    if it and not self[it] then self[it]=it end
+    return self
+  end,
+  __call=function(self, ...)
+    assert(is.of.set(self))
+    assert(is.callable(mt(self).__item) or mt(self).__item==nil)
+    return setmetatable({}, getmetatable(self)) .. args(...)
+  end,
+  __concat=function(self, o)
+    assert(is.of.set(self))
+    if is.bulk(o) then for it in iter(o) do _ = self + it end end
+    return self
+  end,
+  __eq=function(a, b)
+    assert(is.similar(a, b), 'require similar objects')
+    return a <= b and b <= a
+  end,
+  __index=function(self, it)
+    if type(it)=='nil' then return nil end
+    assert(is.of.set(self))
+    local __item=mt(self).__item
+    return rawget(self, is.callable(__item) and __item(it) or it)
+  end,
 --  __item=t.fn.noop,
   __iter=function(self)
     local k
@@ -16,52 +43,6 @@ return setmetatable({
       if k~=nil then
         return k
       end
-    end
-  end,
---  __mode='v',
-  __mod=function(self, it) if type(it)=='function' then return self(table.filter(self, it)) end end,
-  __tostring=function(self) return table.concat(table.map(table.iter(self), tostring), "\n") end,
-  __call=function(self, ...)
-    assert(is.of.set(self))
-    assert(is.callable(mt(self).__item) or mt(self).__item==nil)
-    return setmetatable({}, getmetatable(self)) .. args(...)
-  end,
-  __index=function(self, it)
-    if type(it)=='nil' then return nil end
-    assert(is.of.set(self))
-    local __item=mt(self).__item
-    return rawget(self, is.callable(__item) and __item(it) or it)
-  end,
-  __newindex=function(self, it, v)
-    assert(is.of.set(self))
-    if it and is.callable(mt(self).__item) then it=mt(self).__item(it) end
-    if it and v and not self[it] then rawset(self, it, it) end
-  end,
-  __concat=function(self, o)
-    assert(is.of.set(self))
-    if is.bulk(o) then for it in iter(o) do _ = self + it end end
-    return self
-  end,
-  __add=function(self, it)
-    assert(is.of.set(self))
-    if not it then return self end
-    if is.bulk(it) then return self .. it end
-    if it and not self[it] then self[it]=it end
-    return self
-  end,
-  __sub=function(self, it)
-    assert(is.of.set(self))
-    if is.bulk(it) then for o in iter(it) do _=self-o end; return self end
-    if it and is.callable(mt(self).__item) then it=mt(self).__item(it) end
-    if it and self[it] then rawset(self, it, nil) end
-    return self
-  end,
-  __mul=function(self, it)
-    if type(it)=='function' then return self(table.map(self, it)) end
-    if type(it)=='table' then
-      local rv = self(it)
-      for x in pairs(rv) do if not self[x] then _=rv-x end end
-      return rv
     end
   end,
   __le=function(a, b)
@@ -73,8 +54,27 @@ return setmetatable({
     assert(is.similar(a, b), 'require similar objects')
     return a <= b and not (b <= a)
   end,
-  __eq=function(a, b)
-    assert(is.similar(a, b), 'require similar objects')
-    return a <= b and b <= a
+  __mod=function(self, it) if type(it)=='function' then return self(table.filter(self, it)) end end,
+--  __mode='v',
+  __mul=function(self, it)
+    if type(it)=='function' then return self(table.map(self, it)) end
+    if type(it)=='table' then
+      local rv = self(it)
+      for x in pairs(rv) do if not self[x] then _=rv-x end end
+      return rv
+    end
   end,
+  __newindex=function(self, it, v)
+    assert(is.of.set(self))
+    if it and is.callable(mt(self).__item) then it=mt(self).__item(it) end
+    if it and v and not self[it] then rawset(self, it, it) end
+  end,
+  __sub=function(self, it)
+    assert(is.of.set(self))
+    if is.bulk(it) then for o in iter(it) do _=self-o end; return self end
+    if it and is.callable(mt(self).__item) then it=mt(self).__item(it) end
+    if it and self[it] then rawset(self, it, nil) end
+    return self
+  end,
+  __tostring=function(self) return table.concat(table.map(table.iter(self), tostring), "\n") end,
 })
