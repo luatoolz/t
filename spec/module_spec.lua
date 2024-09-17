ebd97055b3363aea31ffffcdad70c76a5a689beb
luatoolz <luatoolz@somahorse.com>
2024-09-17 14:08:22 +0300

    minor module changes

--- spec/module_spec.lua
+++ spec/module_spec.lua
@@ -1,5 +1,5 @@
 describe("module", function()
-  local meta, base, sub, t
+  local meta, module, base, sub, t
   setup(function()
     meta = require "meta"
     t = require "t"
@@ -16,10 +16,7 @@ describe("module", function()
     assert.equal('meta', module(t.module.base('meta')).name)
     assert.equal('meta', module(t.module.base('meta.loader')).name)
 
-    assert.equal('t/def', t.module.base(t.def.auth).name)
-    assert.equal('t/def', t.module.childof(t.def.auth).name)
-
-    assert.equal('t/def/auth', meta.type(t.def.auth))
-    assert.equal('t/def/auth', meta.type(t.def.auth()))
+    assert.equal('meta', t.module.base('meta').base)
+    assert.equal('meta', t.module.base('meta.loader').base)
   end)
 end)
