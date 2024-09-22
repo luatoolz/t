25eae60c62981bbdc05483688c3f75b896d039f5
luatoolz <luatoolz@somahorse.com>
2024-09-22 22:26:50 +0300

    add: t.module.basename

--- lua/t/module.lua
+++ lua/t/module.lua
@@ -5,13 +5,19 @@ local cache = meta.cache
 -- now return meta.module() of base
 -- use .loader computable to get loader
 rawset(meta.module, 'base', function(it)
-  if type(it)=='table' then it=cache.type[it] end
+  if type(it)=='table' then it=cache.type[it] or cache.type[getmetatable(it)] end
   if not t.match.string(it) then return nil end
   return meta.module((meta.module(it) or {}).base)
 end)
 
+rawset(meta.module, 'basename', function(it)
+  if type(it)=='table' then it=cache.type[it] or cache.type[getmetatable(it)] end
+  if not t.match.string(it) then return nil end
+  return (meta.module(it) or {}).base
+end)
+
 rawset(meta.module, 'childof', function(it)
-  if type(it)=='table' then it=cache.type[it] end
+  if type(it)=='table' then it=cache.type[it] or cache.type[getmetatable(it)] end
   if not t.match.string(it) then return nil end
   return meta.module((meta.module(it) or {}).parent)
 end)
