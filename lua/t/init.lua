9408f09dd204c6f80c6037ec9e71a3d284ddd10c
luatoolz <luatoolz@somahorse.com>
2024-09-28 15:00:33 +0300

    add: use patch.d convention: auto load from init

--- lua/t/init.lua
+++ lua/t/init.lua
@@ -1,12 +1,13 @@
 require "compat53"
 require "luassert"
 local meta = require "meta"
+local no=meta.no
 
 if not t then
   t = meta.loader('t') ^ 't'
-  t:assert(meta.no.asserted)
+  t:assert(no.asserted)
   t:init()
-  t:patch()
+  t:patch(no.call)
 end
 
 return t
