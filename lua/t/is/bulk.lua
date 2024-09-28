4afcde1be1f2eaf16c712f4039b3521b1bee3f2d
luatoolz <luatoolz@somahorse.com>
2024-09-28 13:37:49 +0300

    fix: is.bulk

--- lua/t/is/bulk.lua
+++ lua/t/is/bulk.lua
@@ -1,3 +1,3 @@
 local t=t or require "t"
 local is=t.is
-return function(x) return (type(x)=='table' and (is.array(x) or is.set(x) or is.ofarray(x) or is.ofset(x) or is.table.indexed(x))) and true or false end
+return function(x) return (type(x)=='table' and (is.array(x) or is.set(x) or is.ofarray(x) or is.ofset(x) or is.table.iindexed(x) or x.__array)) and true or false end
