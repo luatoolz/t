5c93e4dd897e3d2be02d87ab78b87d06302ff6cb
luatoolz <luatoolz@somahorse.com>
2024-09-28 15:35:39 +0300

    fix: is.bulk minor addons

--- lua/t/is/bulk.lua
+++ lua/t/is/bulk.lua
@@ -1,3 +1,6 @@
 local t=t or require "t"
 local is=t.is
-return function(x) return (type(x)=='table' and (is.array(x) or is.set(x) or is.ofarray(x) or is.ofset(x) or is.table.iindexed(x) or x.__array)) and true or false end
+return function(x) return (type(x)=='table' and (
+  is.array(x) or is.set(x) or is.ofarray(x) or is.ofset(x) or is.table.iindexed(x) or
+  x.__array or (getmetatable(x) or {}).__array or (getmetatable(x) or {}).__arraytype
+)) and true or false end
