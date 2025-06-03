945cb2612dd2f4df938181ecb56baabace19061e
luatoolz <luatoolz@somahorse.com>
2025-06-03 06:30:28 +0300

    t.rex refactoring

--- lua/t/rex/init.lua
+++ lua/t/rex/init.lua
@@ -2,19 +2,67 @@ local rexlib = require 'rex_pcre2'
 local t = require 't'
 local pkg = t.pkg(...)
 local mt = t.gmt
+local call = t.call
+
+local computed = t.mt.computed
 
 local tab = table()
-local default = 'i'
+local default = 'i' -- imsxU
 local config  = rexlib.config()
 local make    = rexlib.maketables()
 mt(make).__metatable = nil
 _ = config
-local some = rexlib.new('some')
 
-mt(some, {
-__call=function(self, subj) subj=subj or ''
-  local rv = table(table.pack(self:tfind(subj)))
-  assert(type(rv)=='table', 'await table, got '..type(rv))
+local function nuller(...) return nil end
+local function topack(...) return table.nulled(table(...)) or nil end
+local function nonempty(s,l) return (s or type(l)~='nil') and s or nil end
+
+return setmetatable({},{
+__computed = {
+  iscomplex = function(self) return self.isindexed or self.isnamed end,
+  isindexed = function(self) local p=self.pattern;  return (type(p)=='string' and (p:match('[^%\\]%([^%?]') or p:match('^%([^%?]'))) and true or nil end,
+  isnamed   = function(self) local p=self.pattern;  return (type(p)=='string' and p:match('%(%?%<[%w_]+%>')) and true or nil end,
+  iswhole   = function(self) local p=self.pattern;  return (type(p)=='string' and p:match('^%^.+%$$')) and true or nil end,
+
+  patterns  = function(self) local p=self.pattern;  return type(p)=='string' and p:gsub('^%^',''):gsub('%$$','') or nil end,
+  startp    = function(self) local p=self.patterns; return type(p)=='string' and '^'..p or nil end,
+  endp      = function(self) local p=self.patterns; return type(p)=='string' and p..'$' or nil end,
+
+  compiled  = function(self) return rexlib.new(self.pattern,  self.options or default) end,
+  compileds = function(self) return rexlib.new(self.patterns, self.options or default) end,
+
+  ltrim     = function(self) return function(subj) return subj and rexlib.gsub(subj, self.startp, '') or nil end end,
+  rtrim     = function(self) return function(subj) return subj and rexlib.gsub(subj, self.endp, '') or nil end end,
+  trim      = function(self) return function(subj) return subj and rexlib.gsub(rexlib.gsub(subj, self.startp, ''), self.endp, '') or nil end end,
+
+  match     = function(self) return function(subj, ...) local p=self.compiled;  if p then return rexlib.match(subj or '', p, ...) end; return nil end end,
+  matchs    = function(self) return function(subj, ...) local p=self.compileds; if p then return rexlib.match(subj or '', p, ...) end; return nil end end,
+  gsub      = function(self) return function(subj, repl, ...) local p=self.compileds;  if (p and subj) then return rexlib.gsub(subj, p, repl or '', ...) end; return nil end end,
+  find      = function(self) return function(subj, ...) local p=self.compileds; if p then return rexlib.find(subj or '', p, ...) end; return nil end end,
+  tfind     = function(self) return function(subj, ...) local p=self.compileds; if p then return topack(p:tfind(subj or '', ...)) end; return nil end end,
+},
+__computable = {
+  options   = function(self) return #self>0 and self[2] or nil end,
+  pattern   = function(self) return #self>0 and self[1] or nil end,
+
+  gmatch    = function(self) local p=self.compileds; return function(subj) return type(subj)=='string' and rexlib.gmatch(subj, p) or nuller end end,
+  count     = function(self) local p=self.compileds; return function(subj) return type(subj)=='string' and rexlib.count(subj, p)  or 0 end end,
+  split     = function(self) local p=self.compileds; return function(subj) return type(subj)=='string' and call.lift(rexlib.split(subj, p),nonempty) or nuller end end,
+  splitz    = function(self) return function(subj) return self.split(string.null(self.trim(subj))) end end,
+},
+__name='rex',
+__tostring=function(self) return table.concat(self, ',') end,
+__index = function(self, k) return #self>0 and computed(self, k) or table.save(self, k, self(pkg(self, k))) end,
+__call = function(self, p, opts)
+  if type(p)=='table' then p, opts = table.unpack(p) end
+  if type(p)=='nil' then return nil end
+  assert(type(p)=='string', 'pattern is not a string: '..type(p))
+  assert(type(opts)=='number' or type(opts)=='string' or type(opts)=='nil', 'opts is not a number/string/nil: '..type(opts))
+  if #self==0 then return setmetatable({p, opts}, getmetatable(self)) end
+
+  local subj=p or ''
+  local rv = self.tfind(subj)
+  if not rv then return rv end
   if type(rv[3])=='table' then
     rv[3] = setmetatable(rv[3], getmetatable(tab))
   end
@@ -23,45 +71,4 @@ __call=function(self, subj) subj=subj or ''
   end
   return table.nulled(table()..table.nulled(rv[3])) or nil
 end,
-}, {__metatable=false})
-
-return pkg ^ function(x)
-  if type(x)=='string' then return rexlib.new(x, default) end
-  if type(x)=='table' then return rexlib.new(table.unpack(x)) end
-  return nil
-end
-
---[[
-https://rrthomas.github.io/lrexlib/manual.html#match
-
-flags
-new
-  rex.new (patt, [cf], [larg...])
-
-match
-  rex.match (subj, patt, [init], [cf], [ef], [larg...])
-  r:match (subj, [init], [ef])
-
-find
-  rex.find (subj, patt, [init], [cf], [ef], [larg...])
-  r:find (subj, [init], [ef])
-
-gmatch
-  rex.gmatch (subj, patt, [cf], [ef], [larg...])
-
-gsub
-  rex.gsub (subj, patt, repl, [n], [cf], [ef], [larg...])
-
-split
-  rex.split (subj, sep, [cf], [ef], [larg...])
-
-count
-  rex.count (subj, patt, [cf], [ef], [larg...])
-
-tfind
-  r:tfind (subj, [init], [ef])
-
-exec
-  r:exec (subj, [init], [ef])
-
---]]
+})
